1398d781d58409e1eb80ed875ef9da230a1ad3bf
Dendedabreakquiaria <Andresilvis@gmail.com>
2024-08-20 20:20:19 -0300

    Aula sobre POO e outras alterações.

---
+++ P.O.O/main.py
@@ -0,0 +1,28 @@
+#1)CLASSE : estrutura de dados que organiza informaçoes e comporta meno ex:carro
+    #ex:carro
+#SUBCLASSE : utilitarios,caminhoes.
+
+#2)ATRIBUTOS: propriedades,caracteristicas
+
+    #EX:potencia,velociade,cambio,direção,freio,etc,
+
+#4)OBJETO : Dados criados baseados em uma classe
+   #EX : Lamborghini ,Fusca , Porshe , Kombi ...(etc)
+#Instancia : Cria objetos baseados em uma classe
+
+                                                            #Lista de codigos
+
+                                                            #Class carro
+
+                                                            #def_init__(self,potencia,cambio)
+
+                                                            #self.potencia=potencia
+
+                                                            #self.cambio=cambio
+
+                                                            #def acelerar (se/f)
+
+                                                            #Porshe = carro(1500,"automatio"
+                                                            #porshe.Acelerar( )
+
+
